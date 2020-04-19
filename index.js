52c7c984f453a971746782c3e03cb934bb2d4e00
jayakumarverana <44598981+jayakumarverana@users.noreply.github.com>
2020-04-19 07:50:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/github", (req, res) => {
+  console.log("req is", req);
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
